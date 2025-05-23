3c6eefe798c449183def78406d4957d067ebc246
joserojascascopy <joserojascascopy@hotmail.com>
2025-05-23 19:19:20 -0300

    crear las rutas (url) iniciales

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,35 @@
 <?php
 
-use Controllers\HomeController;
+use Controllers\LoginController;
 use MVC\Router;
 
 $router = new Router;
 
-$router->get('/', [HomeController::class, 'index']);
+// Login
+
+$router->get('/', [LoginController::class, 'login']);
+$router->post('/', [LoginController::class, 'login']);
+
+// Logout
+
+$router->get('/logout', [LoginController::class, 'logout']);
+
+// Crear cuenta
+
+$router->get('/crear', [LoginController::class, 'crear']);
+$router->post('/crear', [LoginController::class, 'crear']);
+
+// Olvide mi contraseña
+
+$router->get('/olvide', [LoginController::class, 'olvide']);
+$router->post('/olvide', [LoginController::class, 'olvide']);
+
+// Reestablecer contraseña
+
+$router->get('/reestablecer', [LoginController::class, 'reestablecer']);
+$router->post('/reestablecer', [LoginController::class, 'reestablecer']);
+
+// Confirmar cuenta
+
+$router->get('/mensaje', [LoginController::class, 'mensaje']);
+$router->get('/confirmar', [LoginController::class, 'confirmar']);
