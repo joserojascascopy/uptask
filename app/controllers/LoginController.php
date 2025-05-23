6f576e1809bde85660c9f073755c22e0787341bd
joserojascascopy <joserojascascopy@hotmail.com>
2025-05-23 20:18:07 -0300

    crear la vista de login.php

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -1,14 +1,19 @@
 <?php
 
 namespace Controllers;
+use MVC\Router;
 
 class LoginController {
-    public static function login() {
-        echo "Desde login";
+    public static function login(Router $router) {
 
         if($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         }
+
+        // Render de la vista Login
+        $router->render('auth/login', [
+            'titulo' => 'Login'
+        ]);
     }
 
     public static function logout() {
--- app/views/main.php
+++ app/views/main.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/assets/css/styles.css">
-    <title>JHP - Framework</title>
+    <title>Uptask | <?php echo $titulo; ?></title>
 </head>
 
 <body>
