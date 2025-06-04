f3436213ee464fcf170c1aff33ed7bc0a0dc4b47
joserojascascopy <joserojascascopy@hotmail.com>
2025-06-03 21:16:30 -0300

    cambiar la conexión a la DB de PDO a mysqli

--- config/database.php
+++ config/database.php
@@ -2,8 +2,31 @@
 
 use Dotenv\Dotenv;
 
-function connect(): PDO {
+// function connect(): PDO {
 
+//     if (!isset($_ENV['DB_HOST'])) {
+//         $dotenv = Dotenv::createImmutable(dirname(__DIR__));
+//         $dotenv->load();
+//     }
+
+//     $host = $_ENV['DB_HOST'];
+//     $dbname = $_ENV['DB_NAME'];
+//     $usuario = $_ENV['DB_USER'];
+//     $password = $_ENV['DB_PASS'];
+
+//     $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
+
+//     try {
+//         return new \PDO($dsn, $usuario, $password, [
+//             \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+//             \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+//         ]);
+//     } catch (\PDOException $e) {
+//         die("Error de conexión: " . $e->getMessage());
+//     }
+// }
+
+function connect() {
     if (!isset($_ENV['DB_HOST'])) {
         $dotenv = Dotenv::createImmutable(dirname(__DIR__));
         $dotenv->load();
@@ -13,15 +36,12 @@ function connect(): PDO {
     $dbname = $_ENV['DB_NAME'];
     $usuario = $_ENV['DB_USER'];
     $password = $_ENV['DB_PASS'];
+    
+    $db = new mysqli($host, $usuario, $password, $dbname);
 
-    $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
-
-    try {
-        return new \PDO($dsn, $usuario, $password, [
-            \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
-            \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
-        ]);
-    } catch (\PDOException $e) {
-        die("Error de conexión: " . $e->getMessage());
+    if (!$db) {
+        echo "No se ha podido establecer la conexión con la base de datos";
     }
+
+    return $db;
 }
