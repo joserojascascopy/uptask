49228e550b11296e0c12f448751ba23eb9d15d85
joserojascascopy <joserojascascopy@hotmail.com>
2025-07-23 22:02:19 -0300

    update sidebar.js

--- app/assets/js/sidebar.js
+++ app/assets/js/sidebar.js
@@ -3,11 +3,11 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 function sidebar() {
-    const menuMobile = document.querySelector('.menu-mobile');
+    const menuMobileBtn = document.querySelector('.menu-mobile');
     const sidebar = document.querySelector('.sidebar');
     const overlay = document.querySelector('.overlay');
 
-    menuMobile.addEventListener('click', function () {
+    menuMobileBtn.addEventListener('click', function () {
         sidebar.classList.add('mostrar');
         overlay.classList.add('activo');
     });
@@ -17,8 +17,8 @@ function sidebar() {
         sidebar.classList.remove('mostrar');
     });
 
-    document.addEventListener('keydown', function(e) {
-        if(e.key === 'Escape') {
+    document.addEventListener('keydown', function (e) {
+        if (e.key === 'Escape') {
             overlay.classList.remove('activo');
             sidebar.classList.remove('mostrar');
         }
